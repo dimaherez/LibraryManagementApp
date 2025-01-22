736fa8d409eda56b20d869687b84abc8e4b459fe
dimaherez <dimaherez@gmail.com>
2025-01-22 13:12:45 +0200

    Task 1_2 update Group books by Genre. Add sorting by title, release date, or price. For each sorting order show available first Count total books by genre, by author show most popular(borrow count) books. Filter books by author or availability. Extract all unique authors. Create a summary report for borrowed books count by genre. trending(last 5 min or another testable time) author

--- app/src/main/java/com/example/librarymanagementapp/Library.kt
+++ app/src/main/java/com/example/librarymanagementapp/Library.kt
@@ -1,32 +1,37 @@
 package com.example.librarymanagementapp
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+
 data class Book(
     val title: String,
     val genre: Genre,
-    val author: String
+    val author: String,
+    val releaseDate: LocalDate,
+    val price: Float,
+    var isAvailable: Boolean,
+    var borrowCount: Int,
+    var availableCount: Int
+)
+
+data class BookOrder(
+    val book: Book,
+    val orderDateTime: LocalDateTime
 )
 
 enum class Genre {
-    ROMANCE,
+    FICTION,
     FANTASY,
     SCIENCE_FICTION,
-    PARANORMAL,
-    HORROR,
+    MYSTERY,
+    BIOGRAPHY,
 }
 
 object LibraryDB {
-    private val books = mutableListOf(
-        Book("Harry Potter and the Sorcerer's Stone", Genre.FANTASY, "J.K. Rowling"),
-        Book("Dune", Genre.SCIENCE_FICTION, "Frank Herbert"),
-        Book("The Da Vinci Code", Genre.HORROR, "Dan Brown"),
-        Book("The Shining", Genre.HORROR, "Stephen King"),
-        Book("The Girl with the Dragon Tattoo", Genre.FANTASY, "Stieg Larsson"),
-        Book("Normal People", Genre.SCIENCE_FICTION, "Sally Rooney")
-    )
+    private val books = initBooks()
+    private val bookOrders = initBookOrders()
 
-    private val borrowedBooks: MutableList<Book> = mutableListOf()
-
-    fun getAvailableBooks(): List<Book>? = books
+    fun getAvailableBooks(): List<Book>? = books.filter { it.isAvailable }
 
     fun addBook(book: Book): Book {
         books.add(book)
@@ -34,29 +39,166 @@ object LibraryDB {
     }
 
     fun findBookByTitle(title: String): Book? =
-        books.find { it.title.lowercase() == title.lowercase() }
+        books.find { it.title.equals(title, ignoreCase = true) }
 
-    fun findBookByGenre(genre: Genre): List<Book> = books.filter { it.genre == genre }
+    fun findBooksByGenre(genre: Genre): List<Book> = books.filter { it.genre == genre }
 
     fun borrowBook(title: String): Book? {
         val book = findBookByTitle(title)
 
         if (book !== null) {
-            books.remove(book)
-            borrowedBooks.add(book)
+            book.borrowCount += 1
+            book.availableCount -= 1
+            book.isAvailable = (book.availableCount != 0)
+
+            bookOrders.add(BookOrder(book, LocalDateTime.now()))
         }
         return book
     }
 
     fun returnBook(title: String): Book? {
-        val book = borrowedBooks.find { it.title == title }
+        val book = books.find { it.title == title }
 
-        if (book !== null) {
-            borrowedBooks.remove(book)
-            books.add(book)
+        if (book?.isAvailable?.not() == true) {
+            book.availableCount += 1
+            book.isAvailable = true
         }
 
         return book
+    }
+
+    // Group books by Genre
+    fun groupByGenre(): Map<Genre, List<Book>> = books.groupBy { it.genre }
+
+    // Group books by availability
+    private fun groupByAvailability() = books.groupBy { it.isAvailable }
+
+    //Add sorting by title, release date, or price. For each sorting order show available first
+    fun sortedByTitleAscending() = groupByAvailability().flatMap { entry -> entry.value.sortedBy { it.title } }
+    fun sortedByTitleDescending() = groupByAvailability().flatMap { entry -> entry.value.sortedByDescending { it.title } }
+
+    fun sortedByReleaseDateAscending() = groupByAvailability().flatMap { entry -> entry.value.sortedBy { it.releaseDate } }
+    fun sortedByReleaseDateDescending() = groupByAvailability().flatMap { entry -> entry.value.sortedByDescending { it.releaseDate } }
+
+    fun sortedByPriceAscending() = groupByAvailability().flatMap { entry -> entry.value.sortedBy { it.price } }
+    fun sortedByPriceDescending() = groupByAvailability().flatMap { entry -> entry.value.sortedByDescending { it.price } }
+
+    // Count total books by genre
+    fun countByGenre(genre: Genre) = books.count { it.genre == genre }
+
+    // Count total books by author
+    fun countByAuthor(author: String) = books.count { it.author == author }
+
+    // show most popular(borrow count) books
+    fun mostPopular() = books.sortedBy { it.borrowCount }.takeLast(3)
+
+    // Filter books by author
+    fun filterByAuthor(author: String) = books.filter { it.author == author }
+
+    // Filter books by availability
+    fun filterByAvailability(isAvailable: Boolean) = books.filter { it.isAvailable == isAvailable }
+
+    // Extract all unique authors
+    fun getUniqueAuthors() = books.map { it.author }.toSet()  // distinct()
+
+    // Create a summary report for borrowed books count by genre
+    fun reportByGenre(): Map<Genre, Int> = books.groupingBy { it.genre }.eachCount()
+
+    // trending(last 5 min or another testable time) author
+    fun trendingAuthor(): String {
+        return bookOrders
+            .filter { it.orderDateTime.isAfter(LocalDateTime.now().minusMinutes(5)) }
+            .groupingBy { it.book.author }
+            .eachCount()
+            .maxBy { it.value }
+            .key
+    }
+
+
+    private fun initBooks(): MutableList<Book> {
+        return mutableListOf(
+            Book(
+                title = "The Great Gatsby",
+                genre = Genre.FICTION,
+                author = "F. Scott Fitzgerald",
+                releaseDate = LocalDate.of(1925, 4, 10),
+                price = 10.99f,
+                isAvailable = true,
+                borrowCount = 5,
+                availableCount = 5
+            ),
+            Book(
+                title = "The Da Vinci Code",
+                genre = Genre.MYSTERY,
+                author = "Dan Brown",
+                releaseDate = LocalDate.of(2003, 3, 18),
+                price = 15.99f,
+                isAvailable = true,
+                borrowCount = 12,
+                availableCount = 5
+            ),
+            Book(
+                title = "A Brief History of Time",
+                genre = Genre.FICTION,
+                author = "Stephen Hawking",
+                releaseDate = LocalDate.of(1988, 6, 1),
+                price = 20.99f,
+                isAvailable = false,
+                borrowCount = 8,
+                availableCount = 5
+            ),
+            Book(
+                title = "Harry Potter and the Philosopher's Stone",
+                genre = Genre.FANTASY,
+                author = "J.K. Rowling",
+                releaseDate = LocalDate.of(1997, 6, 26),
+                price = 25.99f,
+                isAvailable = true,
+                borrowCount = 20,
+                availableCount = 5
+            ),
+            Book(
+                title = "Dune",
+                genre = Genre.SCIENCE_FICTION,
+                author = "Frank Herbert",
+                releaseDate = LocalDate.of(1965, 8, 1),
+                price = 18.99f,
+                isAvailable = true,
+                borrowCount = 15,
+                availableCount = 5
+            ),
+            Book(
+                title = "Steve Jobs",
+                genre = Genre.BIOGRAPHY,
+                author = "Walter Isaacson",
+                releaseDate = LocalDate.of(2011, 10, 24),
+                price = 30.99f,
+                isAvailable = false,
+                borrowCount = 10,
+                availableCount = 5
+            ),
+            Book(
+                title = "Harry Potter and the Chamber of Secrets",
+                genre = Genre.FANTASY,
+                author = "J.K. Rowling",
+                releaseDate = LocalDate.of(1998, 7, 2),
+                price = 26.99f,
+                isAvailable = true,
+                borrowCount = 18,
+                availableCount = 5
+            )
+        )
+    }
+
+    private fun initBookOrders(): MutableList<BookOrder> {
+        return mutableListOf(
+            BookOrder(books.random(), LocalDateTime.now().minusMinutes(10)),
+            BookOrder(books.random(), LocalDateTime.now().minusMinutes(7)),
+            BookOrder(books.random(), LocalDateTime.now().minusMinutes(5)),
+            BookOrder(books.random(), LocalDateTime.now().minusMinutes(3)),
+            BookOrder(books.random(), LocalDateTime.now().minusMinutes(3)),
+            BookOrder(books.random(), LocalDateTime.now().minusMinutes(2)),
+        )
     }
 }
 
--- app/src/main/java/com/example/librarymanagementapp/Main.kt
+++ app/src/main/java/com/example/librarymanagementapp/Main.kt
@@ -1,26 +1,41 @@
 package com.example.librarymanagementapp
 
+import java.time.LocalDate
+
 fun main() {
 
-    println(loadBooks())
+    loadBooks()
 
     println(
         "\n\nAdded a book: ${
             LibraryDB.addBook(
                 Book(
-                    "The Hobbit",
-                    Genre.FANTASY,
-                    " J . R . R . Tolkien "
+                    title = "To Kill a Mockingbird",
+                    genre = Genre.FICTION,
+                    author = "Harper Lee",
+                    releaseDate = LocalDate.of(1960, 7, 11),
+                    price = 12.99f,
+                    isAvailable = true,
+                    borrowCount = 0,
+                    availableCount = 5
                 )
             )
         }"
     )
 
-    println("\n\nFound a book by title: ${LibraryDB.findBookByTitle("The Shining")}")
-    println("Found a book by genre ${LibraryDB.findBookByGenre(Genre.FANTASY)}")
+//    println("\n\nFound a book by title: ${LibraryDB.findBookByTitle("The Shining")}")
+//    println("Found a book by genre ${LibraryDB.findBookByGenre(Genre.FANTASY)}")
+//
+//    println("\n\nBorrowed a book: ${LibraryDB.borrowBook("The Hobbit")}")
+//    println("Returned a book: ${LibraryDB.returnBook("The Hobbit")}")
 
-    println("\n\nBorrowed a book: ${LibraryDB.borrowBook("The Hobbit")}")
-    println("Returned a book: ${LibraryDB.returnBook("The Hobbit")}")
+
+//    println("Sorted By Title: ${LibraryDB.sortedByTitleAscending().forEach(::println)}")
+
+//    println("Report by genre: ${LibraryDB.reportByGenre()}")
+
+//    println("Trending author: ${LibraryDB.trendingAuthor()}")
+
 }
 
 fun loadBooks() {
